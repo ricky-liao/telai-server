535f3fa1d31e2f10bcdc5dc116fd2dacc4ade5ef
Ricky Liao <liaoric@uw.edu>
2024-01-20 18:16:31 -0800

    initial socket io configs

---
+++ src/api/controllers/mainController.ts
@@ -0,0 +1,17 @@
+import {
+  ConnectedSocket,
+  OnConnect,
+  SocketController,
+  SocketIO,
+} from "socket-controllers";
+import { Socket, Server } from "socket.io";
+
+@SocketController()
+export class MainController {
+  @OnConnect()
+  public onConnection(
+    @ConnectedSocket() socket: Socket,
+    @SocketIO() io: Server
+  ) {
+  }
+}
